a401e51fd3e3c175010514b44480b4ad20e0ecbc
Ayman-Yahia <mohmmed20k@gmail.com>
2025-05-13 21:15:34 +0300

    add font

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,29 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import * as Updates from "expo-updates";
+import { useEffect } from "react";
+import "react-native-reanimated";
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+  const [loaded, error] = useFonts({
+    "Main-Bold": require("../assets/fonts/Almarai-Bold.ttf"),
+    "Main-Regular": require("../assets/fonts/Almarai-Regular.ttf"),
+    "Main-SemiBold": require("../assets/fonts/Almarai-SemiBold.ttf"),
   });
+
+  useEffect(() => {
+    if (error) {
+      Updates.reloadAsync();
+    }
+  }, [error]);
 
   if (!loaded) {
     // Async font loading only occurs in development.
@@ -18,7 +31,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
