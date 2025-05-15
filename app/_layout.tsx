5af8b94956df25a24c6b13c44170e75a568ed488
Ayman-Yahia <mohmmed20k@gmail.com>
2025-05-15 20:27:59 +0300

    add swr config

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,12 @@
 import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+import NetInfo from "@react-native-community/netinfo";
 import {
   DarkTheme,
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import axios from "axios";
+import { HTTP_STATUSES } from "constants/status";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -11,11 +14,13 @@ import { StatusBar } from "expo-status-bar";
 import * as Updates from "expo-updates";
 import { useColorScheme } from "hooks/useColorScheme";
 import { useCallback, useEffect } from "react";
+import { AppState, AppStateStatus } from "react-native";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import "react-native-reanimated";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { RecoilRoot } from "recoil";
 import RecoilNexus from "recoil-nexus";
+import { SWRConfig } from "swr";
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -44,11 +49,59 @@ export default function RootLayout() {
     <SafeAreaProvider onLayout={onLayoutRootView}>
       <RecoilRoot>
         <RecoilNexus />
-        <GestureHandlerRootView style={{ flex: 1 }}>
-          <BottomSheetModalProvider>
-            <RootLayoutNav />
-          </BottomSheetModalProvider>
-        </GestureHandlerRootView>
+        <SWRConfig
+          value={{
+            fetcher: (url: string) => axios.get(url).then((res) => res.data),
+            onErrorRetry: (error, key) => {
+              if (error.statusCode === HTTP_STATUSES.NOT_FOUND) return;
+            },
+            provider: () => new Map(),
+            isVisible: () => true,
+            initReconnect(callback) {
+              // Subscribe to network state updates
+              const unsubscribe = NetInfo.addEventListener((state) => {
+                // Trigger revalidation if the network is reachable
+                if (state.isInternetReachable) {
+                  callback();
+                }
+              });
+              // Unsubscribe from the event listener
+              return () => {
+                unsubscribe();
+              };
+            },
+            initFocus(callback) {
+              let appState = AppState.currentState;
+
+              const onAppStateChange = (nextAppState: AppStateStatus) => {
+                /* If it's resuming from background or inactive mode to active one */
+                if (
+                  appState.match(/inactive|background/) &&
+                  nextAppState === "active"
+                ) {
+                  callback();
+                }
+                appState = nextAppState;
+              };
+
+              // Subscribe to the app state change events
+              const subscription = AppState.addEventListener(
+                "change",
+                onAppStateChange
+              );
+
+              return () => {
+                subscription.remove();
+              };
+            },
+          }}
+        >
+          <GestureHandlerRootView style={{ flex: 1 }}>
+            <BottomSheetModalProvider>
+              <RootLayoutNav />
+            </BottomSheetModalProvider>
+          </GestureHandlerRootView>
+        </SWRConfig>
       </RecoilRoot>
     </SafeAreaProvider>
   );
